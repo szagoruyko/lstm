5e83b253a0984117b3bd9ce695afec5a57ece9ec
Sergey Zagoruyko <zagoruyko2@gmail.com>
2015-07-06 14:25:19 +0200

    oxnn lstm

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@
 ----  LICENSE file in the root directory of this source tree. 
 ----
 require('cunn')
+require('oxnn')
+oxnn.InitCuda()
 require('nngraph')
 require('base')
 local ptb = require('data')
@@ -48,32 +50,6 @@ local state_train, state_valid, state_test
 local model = {}
 local paramx, paramdx
 
-local function lstm(x, prev_c, prev_h)
-  -- Calculate all four gates in one go
-  local i2h = nn.Linear(params.rnn_size, 4*params.rnn_size)(x)
-  local h2h = nn.Linear(params.rnn_size, 4*params.rnn_size)(prev_h)
-  local gates = nn.CAddTable()({i2h, h2h})
-  
-  -- Reshape to (batch_size, n_gates, hid_size)
-  -- Then slize the n_gates dimension, i.e dimension 2
-  local reshaped_gates =  nn.Reshape(4,params.rnn_size)(gates)
-  local sliced_gates = nn.SplitTable(2)(reshaped_gates)
-  
-  -- Use select gate to fetch each gate and apply nonlinearity
-  local in_gate          = nn.Sigmoid()(nn.SelectTable(1)(sliced_gates))
-  local in_transform     = nn.Tanh()(nn.SelectTable(2)(sliced_gates))
-  local forget_gate      = nn.Sigmoid()(nn.SelectTable(3)(sliced_gates))
-  local out_gate         = nn.Sigmoid()(nn.SelectTable(4)(sliced_gates))
-
-  local next_c           = nn.CAddTable()({
-      nn.CMulTable()({forget_gate, prev_c}),
-      nn.CMulTable()({in_gate,     in_transform})
-  })
-  local next_h           = nn.CMulTable()({out_gate, nn.Tanh()(next_c)})
-
-  return next_c, next_h
-end
-
 local function create_network()
   local x                = nn.Identity()()
   local y                = nn.Identity()()
@@ -86,7 +62,10 @@ local function create_network()
     local prev_c         = split[2 * layer_idx - 1]
     local prev_h         = split[2 * layer_idx]
     local dropped        = nn.Dropout(params.dropout)(i[layer_idx - 1])
-    local next_c, next_h = lstm(dropped, prev_c, prev_h)
+    local next_h, next_c = oxnn.ModelUtil.LSTMCell12Graph(prev_h, prev_c, dropped,
+        nn.Linear(params.rnn_size, 4 * params.rnn_size),
+        nn.Linear(params.rnn_size, 4 * params.rnn_size),
+        nn.Identity())
     table.insert(next_s, next_c)
     table.insert(next_s, next_h)
     i[layer_idx] = next_h
